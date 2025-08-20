1e78f22b9f89be2d9b3f3c061df6e93c5d217126
Sandeeptha Madan <msandee@Mac.home.local>
2025-08-19 22:13:00 -0500

    build: publish for pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // because your site will live at /poker-loca/ under your username
+  base: '/poker-loca/',
 })
